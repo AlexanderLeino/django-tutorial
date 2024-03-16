917981a5c98d0ad194a28d32c38793552af19c33
Alexander Leino <Alexander.Leino@yahoo.com>
2024-03-16 15:59:56 -0400

    first commit

---
+++ hello/views.py
@@ -0,0 +1,18 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.timezone import datetime
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name): 
+    return render(
+         request, 
+         'hello/hello_there.html',
+         {
+              'name': name,
+              'date': datetime.now()
+         }
+    )
